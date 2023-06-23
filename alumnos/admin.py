f06c6e598012aa1e93caa130584fd274442cbf2f
Hellraiserr2 <ex.tapia@duocuc.cl>
2023-06-23 06:26:01 -0400

    base de datos mas o menos
    me falta arreglarla pero falta poco

--- alumnos/admin.py
+++ alumnos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Planta, Categoria
+
+# Registra el modelo Planta en el sitio de administración
+@admin.register(Planta)
+class PlantaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'precio')  # Campos a mostrar en la lista de objetos
+    list_filter = ('categoria',)  # Filtros disponibles en la lista
+    search_fields = ('nombre', 'descripcion')  # Campos de búsqueda
+    prepopulated_fields = {'slug': ('nombre',)}  # Genera el slug automáticamente
+
+# Registra el modelo Categoria en el sitio de administración
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)  # Campos a mostrar en la lista de objetos
+    search_fields = ('nombre',)  # Campos de búsqueda
--- alumnos/models.py
+++ alumnos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Planta(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    imagen = models.ImageField(upload_to='plantas/')
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+class Pedido(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    direccion = models.TextField()
+
+class DetallePedido(models.Model):
+    pedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
+    planta = models.ForeignKey('Planta', on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
